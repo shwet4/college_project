0fdbf938d451de277ad95c0f70c678c25fdab76a
shwet4 <124236108+shwet4@users.noreply.github.com>
2023-03-31 12:16:18 +0530

    Delete readexcel.py

--- AppiumTest/readexcel.py
+++
@@ -1,7 +0,0 @@
-import xlrd
-
-workbook=xlrd.open_workbook("C:\\Users\\158260\\PycharmProjects\\testscript\\AppiumTest\\excel\\Book1.xlsx")
-sheet=workbook.sheet_by_name('Sheet1')
-
-row_count=sheet.nrows
-print("total number of rows",row_count)
